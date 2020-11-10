87c47b44cd90b3b9f9db3351490734d183a37d0e
David <davidryder@yahoo.com>
2020-11-10 10:10:42 -0800

    Initial

---
+++ test-app0/index.js
@@ -0,0 +1,14 @@
+console.log("Starting");
+require("appdynamics").profile({
+  reuseNode: 'true',
+  debug: 'true'
+});
+var http = require("http");
+
+var n = 1;
+http.createServer(function (request, response) {
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   response.end(n.toString());
+   n++;
+}).listen(process.env.APP_LISTEN_PORT);
+console.log('Server 2 running at http://127.0.0.1:' + process.env.APP_LISTEN_PORT + '/');
---
+++ test-app0/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+
+nvm install v10.18.0
+nvm install v12.19.0
+
+
