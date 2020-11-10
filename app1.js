661780474a60017cf19857a9f445a2094782f9fc
David <davidryder@yahoo.com>
2020-11-10 11:17:21 -0800

    rm

--- app1.js
+++
@@ -1,9 +0,0 @@
-var http = require("http");
-
-var n = 1;
-http.createServer(function (request, response) {
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   response.end(n.toString());
-   n++;
-}).listen(process.env.APP_LISTEN_PORT);
-console.log('Server running at http://127.0.0.1:' + process.env.APP_LISTEN_PORT + '/');
--- appd-start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#
-# Install AppDynamics, Start teh Agent and the original Application
-# Add additional container startup scripting here as necessary
-#
-npm install appdynamics@next &&
-node appd-start.js
